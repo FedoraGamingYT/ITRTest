9c75955b84cb0ee3672737ba94beb21e1443b2e1
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-13 16:43:25 +0200

    Added questions table

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 -- https://pastebin.com/HiLgBvg2 - Update the version when a new update has been sent to the module.
 local mainOS = {}
-mainOS.loadOS = function()
+local Questions = {
+  ["What is this?"] = {
+    answer = "This is a system made by Fedora."
+  },
+  ["Where can I find some information?"] = {
+    answer = "You can find information for this software in the information tab."
+  }
+}
+function mainOS.loadOS() 
   print("Loaded latest version of the os!")
 end
 return mainOS
