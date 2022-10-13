9e895ade479ea84df5b546d54f23c6081e69e82e
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-13 15:57:43 +0200

    Fixed Function

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 -- https://pastebin.com/HiLgBvg2 - Update the version when a new update has been sent to the module.
-_G.loadOS = function()
+local mainOS = {}
+mainOS.loadOS = function()
   print("Loaded latest version of the os!")
 end
+return mainOS
