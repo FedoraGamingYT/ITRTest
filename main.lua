213c4b09d410898daf5939b82717fbc8dbd8bd57
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-13 21:17:29 +0200

    Changed loadOS to print Penis

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- https://pastebin.com/HiLgBvg2 - Update the version when a new update has been sent to the module.
 local mainOS = {}
 function mainOS.loadOS() 
-  print("Loaded latest version of the os!")
+  print("penis")
 end -- test
 return mainOS
