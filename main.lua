9496d177c9e51a54df1cb386931623f00ca88ac0
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-14 10:32:54 +0200

    work in progress new function

--- main.lua
+++ main.lua
@@ -1,6 +1,12 @@
 -- https://pastebin.com/HiLgBvg2 - Update the version when a new update has been sent to the module.
 local mainOS = {}
+local dir = nil
+local loaded = true
+function mainOS.notify()
+
+end
 function mainOS.loadOS() 
+  dir = script.Parent
   print("Loaded latest version of IFE software.")
 end
 return mainOS
