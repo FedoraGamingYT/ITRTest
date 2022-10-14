d8d7f53a6387baf89c7a1addef6909c91547d826
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-14 10:45:55 +0200

    fixed so it opens on load

--- main.lua
+++ main.lua
@@ -5,14 +5,15 @@ local loaded = true
 function mainOS.notify() -- work in progress
 
 end
-function mainOS.loadOS()
-  dir = script.Parent
-  print("Loaded latest version of IFE software.")
-end
 function mainOS.close()
   dir.Visible = false
 end
 function mainOS.open()
   dir.Visible = true
+end
+function mainOS.loadOS()
+  dir = script.Parent
+  print("Loaded latest version of IFE software.")
+  mainOS.open()
 end
 return mainOS
