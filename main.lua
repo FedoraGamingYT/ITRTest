d85c0e0f4b1c46920f02b7830871c17307308c3e
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-14 10:52:24 +0200

    Added classname checking and loaded value to set to true & false depending on the loaded or unloaded status.

--- main.lua
+++ main.lua
@@ -1,27 +1,35 @@
 -- https://pastebin.com/HiLgBvg2 - Update the version when a new update has been sent to the module.
 local mainOS = {}
 local dir = nil
-local loaded = true
+local loaded = false
 function mainOS.notify() -- work in progress
 
 end
 function mainOS.close()
+  if dir.ClassName == "Frame" then
   dir.Visible = false
+  else
+    warn("You cannot close the OS in a script outside of a frame!")
+  end
 end
 function mainOS.open()
-  dir.Visible = true
+  if dir.ClassName == "Frame" then
+    dir.Visible = true
+    else
+      warn("You cannot open the OS in a script outside of a frame!")
+    end
 end
 function mainOS.loadOS()
   dir = script.Parent
   print("Loaded latest version of IFE software.")
-  mainOS.open()
+  loaded = true
 end
 function mainOS.unloadOS()
   if loaded == false then
     print("The OS is not loaded!")
   else 
     print("Unloaded IFE software.")
-    mainOS.close()
+    loaded = false
   end
 end
 return mainOS
